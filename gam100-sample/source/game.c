689e1c303a141c5e604fa1ee80c18ac7b4d97083
ohy99 <ohy99@hotmail.com>
2021-09-28 14:30:54 +0800

    Cleaned up main.c and added game.c

---
+++ gam100-sample/source/game.c
@@ -0,0 +1,27 @@
+/*!
+@file game.c
+@author TODO
+@course TODO
+@section TODO
+@tutorial TODO
+@date TODO
+@brief This file contains functions of game init, update, exit
+*//*______________________________________________________________________*/
+
+#include <stdio.h>
+#include "cprocessing.h"
+
+void game_init(void)
+{
+
+}
+
+void game_update(void)
+{
+    
+}
+
+void game_exit(void)
+{
+
+}
