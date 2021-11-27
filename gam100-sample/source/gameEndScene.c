36708a3d1bc731a9ba9968102f534b7e14f4c8c1
greed-k <88334678+greed-k@users.noreply.github.com>
2021-11-27 15:53:45 +0800

    Game End Scene change
    didnt change the code

--- gam100-sample/source/gameEndScene.c
+++ gam100-sample/source/gameEndScene.c
@@ -34,7 +34,7 @@ void gameEnd_init(void)
         CP_Vector_Set(0.5f * screenWidth, 0.5f * screenHeight), 0.0f, CP_Vector_Set(screenWidth, screenHeight));
     Renderer* bgr = RM_AddComponent(bg);
     bgr->renderPriority = PRI_UI;
-    RM_LoadImage(bgr, "Assets/scenes/gameOver.png");
+    RM_LoadImage(bgr, "Assets/scenes/Game_Over.png");
 
 
     GameObject* eButton = GOM_Create2(RECTANGLE, CP_Vector_Set(50 * xScale, 90 * yScale), 0.0f, CP_Vector_Set(BUTTON_WIDTH, BUTTON_HEIGHT));
